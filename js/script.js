ec967b939c9a957515abff1586b4287bad44ef07
Julia <juliasoaresvieira@hotmail.com>
2025-02-26 20:09:23 -0300

    Ajustes funcionalidade do slider, ajustes checkmark

--- js/script.js
+++ js/script.js
@@ -1,9 +1,46 @@
-const inputSlider = document.querySelector(".size__password--slider--field input");
-const slideValue = document.querySelector(".size__password--description--value")
+window.onload = function() {
 
-inputSlider.oninput = (()=>{
-    let value = inputSlider.value;
-    let realValue = inputSlider.value;
+    const inputSlider = document.querySelector(".size__password--slider--field input"); // slider
+    const numberShowed = document.querySelector(".size__password--description--value") // value showed for the size
 
-    slideValue.textContent = value;
-});
+    let valueInputSlider = inputSlider.value;
+    let valueDisplayed = numberShowed.value;
+
+    inputSlider.oninput = (()=>{
+        valueInputSlider = inputSlider.value;
+        numberShowed.textContent = valueInputSlider;
+    });
+
+    const minusButton = document.querySelector("#minus__button");
+    const plusButton = document.querySelector("#plus__button");
+
+    minusButton.onclick = () => {
+        if (valueInputSlider > 0 && valueInputSlider <= 50) {
+            let valueInputSliderNumber = Number(valueInputSlider);
+
+            valueInputSliderNumber -= 1;
+            valueInputSlider = (valueInputSliderNumber).toString();
+
+            numberShowed.textContent = valueInputSlider;
+            inputSlider.value = valueInputSlider;
+
+            return valueInputSlider;
+        }
+    };
+
+    console.log("Valor do input slider antes do clique:", valueInputSlider);
+    plusButton.onclick = () => {
+        if (valueInputSlider >= 0 && valueInputSlider < 50) {           
+            let valueInputSliderNumber = Number(valueInputSlider);
+
+            valueInputSliderNumber += 1;
+            valueInputSlider = (valueInputSliderNumber).toString();
+
+            numberShowed.textContent = valueInputSlider;
+            inputSlider.value = valueInputSlider;
+
+            return valueInputSlider;
+        }
+    };
+
+};
